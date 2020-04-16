2c6431e4062b0bd7b5f80daa3d6704e9e370f046
Martin Chobanyan <chobanyan.martin@gmail.com>
2020-04-16 00:49:22 -0400

    consolidated CategorySampler into Office31

--- domain_adapt/data/office31.py
+++ domain_adapt/data/office31.py
@@ -11,20 +11,38 @@ from torchvision.datasets import ImageFolder
 VALID_DOMAINS = ['amazon', 'dslr', 'webcam']
 
 
-class CategorySampler:
-    def __init__(self, data_dir, n_per_category):
-        self.data_dir = data_dir
-        self.n_per_category = n_per_category
-        self.sampled_files = self.collect_sample()
+class Office31(ImageFolder):
+    def __init__(self, root_dir, domain, source, num_per_category=None):
+        self.domain = domain.lower()
+        if self.domain not in VALID_DOMAINS:
+            raise ValueError(f'"domain" argument must be one of: {VALID_DOMAINS}')
 
-    def collect_sample(self):
+        self.root_dir = root_dir
+        self.source = source
+        self.domain_dir = os.path.join(self.root_dir, self.domain, 'images')
+
+        self.num_per_category = num_per_category
+        if self.num_per_category is None:
+            if source:
+                if domain == 'amazon':
+                    self.num_per_category = 20
+                else:
+                    self.num_per_category = 8
+            else:
+                self.num_per_category = 3
+
+        self.sampled_files = self.sample_per_category()
+
+        super().__init__(root=self.domain_dir, is_valid_file=self.check_file)
+
+    def sample_per_category(self):
         sampled_files = dict()
-        for category in os.listdir(self.data_dir):
-            category_dir = os.path.join(self.data_dir, category)
-            sampled_files[category] = set(random.sample(os.listdir(category_dir), self.n_per_category))
+        for category in os.listdir(self.domain_dir):
+            category_dir = os.path.join(self.domain_dir, category)
+            sampled_files[category] = set(random.sample(os.listdir(category_dir), self.num_per_category))
         return sampled_files
 
-    def __call__(self, filepath):
+    def check_file(self, filepath):
         # isolate the name of the file and its image category
         category_path, filename = os.path.split(filepath)
         category = os.path.basename(category_path)
@@ -32,16 +50,3 @@ class CategorySampler:
         # check if the input file is one of the sampled files for its category
         file_sample = self.sampled_files[category]
         return (filename in file_sample)
-
-
-class Office31(ImageFolder):
-    def __init__(self, root_dir, domain, source, per_category_config=None):
-        if domain.lower() not in VALID_DOMAINS:
-            raise ValueError(f'"domain" argument must be one of: {VALID_DOMAINS}')
-
-        self.root_dir = root_dir
-        self.domain = domain
-        self.source = source
-        self.domain_dir = os.path.join(root_dir, domain, 'images')
-
-        super().__init__(root=self.domain_dir, is_valid_file=CategorySampler(self.domain_dir, 2))
