258b6f559e590e105a2f2fadc3945b894d99e31a
Martin Chobanyan <chobanyan.martin@gmail.com>
2020-04-19 17:13:48 -0400

    added MMd

---
+++ domain_adapt/nn/loss.py
@@ -0,0 +1,21 @@
+import torch
+
+
+def mmd(x1, x2):
+    """Maximum Mean Discrepancy
+
+    This function calculates the MMD between examples from two separate feature distributions.
+
+    Parameters
+    ----------
+    x1: torch.Tensor
+        Examples from a feature space with shape (num_examples, feature_dim)
+    x2: torch.Tensor
+        Examples from a feature space with shape (num_examples, feature_dim)
+
+    Returns
+    -------
+    float
+    """
+    mean_diff = (x1.sum(dim=0) / x1.size(0)) - (x2.sum(dim=0) / x2.size(0))
+    return torch.norm(mean_diff, p=2)
