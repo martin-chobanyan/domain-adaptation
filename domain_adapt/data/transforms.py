3d356ef14f12511cb51dfd8e503b5168e7c6cc20
Martin Chobanyan <chobanyan.martin@gmail.com>
2020-04-19 17:23:58 -0400

    added transforms

---
+++ domain_adapt/data/transforms.py
@@ -0,0 +1,15 @@
+from torchvision.transforms import Compose, Normalize, Resize, ToTensor
+from . import IMAGENET_MEANS, IMAGENET_STDEVS
+
+
+class ImagenetNorm(Normalize):
+    def __init__(self):
+        super().__init__(mean=IMAGENET_MEANS, std=IMAGENET_STDEVS)
+
+
+class DefaultTransform:
+    def __init__(self, img_shape=(256, 256)):
+        self.transform = Compose([Resize(img_shape), ToTensor(), ImagenetNorm()])
+
+    def __call__(self, img):
+        return self.transform(img)
